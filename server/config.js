5c03b6e8988f1562f4add521d091bf783ff6b42f
juliswer <julianswer36@gmail.com>
2022-07-18 22:05:34 -0300

    setting the enviroment variables

--- server/config.js
+++ server/config.js
@@ -1,1 +1,7 @@
-export const PORT = 4000
+import { config } from "dotenv";
+config();
+
+export const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } =
+  process.env;
+
+export const PORT = 4000;
