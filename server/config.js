8f95effd781633910cf441134ad38e22e60578b9
juliswer <julianswer36@gmail.com>
2022-07-23 16:10:46 -0300

    upadting port and sample env

--- server/config.js
+++ server/config.js
@@ -1,7 +1,5 @@
 import { config } from "dotenv";
 config();
 
-export const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT } =
+export const { DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE, DB_PORT, PORT } =
   process.env;
-
-export const PORT = 4000;
