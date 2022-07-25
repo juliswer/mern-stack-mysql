4fcf74f9c5f77b88e09e6cfb11594d4737258eb2
juliswer <julianswer36@gmail.com>
2022-07-25 12:10:06 -0300

    formik set up

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,18 @@ import { Routes, Route } from "react-router-dom";
 import NotFoundPage from "./pages/NotFoundPage";
 import TasksFormPage from "./pages/TasksFormPage";
 import TasksPage from "./pages/TasksPage";
+import Navbar from './components/NavbarComponent'
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<TasksPage />} />
-      <Route path="/new" element={<TasksFormPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<TasksPage />} />
+        <Route path="/new" element={<TasksFormPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
   );
 }
 
--- client/src/pages/TasksFormPage.jsx
+++ client/src/pages/TasksFormPage.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { Form, Formik } from "formik";
 
 function TasksFormPage() {
   return (
-    <div>TasksFormPage</div>
-  )
+    <>
+      <Formik>
+        <Form>
+          <label>Title</label>
+          <input type="text" name="title" placeholder="Write a title" />
+
+          <label>Description</label>
+          <textarea
+            name="description"
+            rows="3"
+            placeholder="Write a description"
+          ></textarea>
+
+          <button>
+            Save
+          </button>
+        </Form>
+      </Formik>
+    </>
+  );
 }
 
-export default TasksFormPage
+export default TasksFormPage;
