df207c1e4758a64cd96d107d9065ce4ed5ae0ecf
juliswer <julianswer36@gmail.com>
2022-07-28 18:12:33 -0300

    endpoints api

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -4,6 +4,18 @@ export const getTasksRequest = async () => {
   return await axios.get("http://localhost:4000/api/tasks");
 };
 
+export const getTaskRequest = async (id) => {
+  return await axios.get(`http://localhost:4000/api/tasks/${id}`);
+};
+
 export const createTaskRequest = async (task) => {
   return await axios.post("http://localhost:4000/api/tasks", task);
+};
+
+export const deleteTaskRequest = async (id) => {
+  return await axios.delete(`http://localhost:4000/api/tasks/${id}`);
+};
+
+export const updateTaskRequest = async (id, task) => {
+  return await axios.put(`http://localhost:4000/api/tasks/${id}`, task);
 };
