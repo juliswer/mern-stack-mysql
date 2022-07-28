3e52cc4dcaa46a7301cf1c7c79a262b2f76174b5
juliswer <julianswer36@gmail.com>
2022-07-27 23:28:32 -0300

    all tasks petition

--- client/src/api/tasks.api.js
+++ client/src/api/tasks.api.js
@@ -1,5 +1,9 @@
 import axios from "axios";
 
+export const getTasksRequest = async () => {
+  return await axios.get("http://localhost:4000/api/tasks");
+};
+
 export const createTaskRequest = async (task) => {
   return await axios.post("http://localhost:4000/api/tasks", task);
 };
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -1,7 +1,30 @@
 import React from "react";
+import { getTasksRequest } from "../api/tasks.api";
 
 function TasksPage() {
-  return <div>TasksPage</div>;
+  const [tasks, setTasks] = React.useState([]);
+
+  React.useEffect(() => {
+    async function loadTasks() {
+      const res = await getTasksRequest();
+      setTasks(res.data.data);
+    }
+    loadTasks();
+  }, []);
+
+  return (
+    <div>
+      <h1>tasks</h1>
+
+      {tasks.map((task) => (
+        <div key={task.id}>
+          <h2>{task.title}</h2>
+          <p>{task.description}</p>
+        </div>
+      ))}
+
+    </div>
+  );
 }
 
 export default TasksPage;
