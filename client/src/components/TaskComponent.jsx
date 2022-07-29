c747920e1e6f58d642f0c51b53fd328fc8d22f0c
juliswer <julianswer36@gmail.com>
2022-07-29 12:51:13 -0300

    delete petition

--- client/src/components/TaskComponent.jsx
+++ client/src/components/TaskComponent.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import { deleteTaskRequest } from "../api/tasks.api";
 
-function TaskComponent({task}) {
+function TaskComponent({ task }) {
+  const handleDelete = async () => {
+    try {
+      await deleteTaskRequest(task.id);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <div>
       <h2>{task.title}</h2>
       <p>{task.description}</p>
       <span>{task.done === 1 ? "✅" : "❌"}</span>
       <span>{task.createdAt}</span>
-      <button>Delete</button>
+      <button onClick={() => handleDelete(task.id)}>Delete</button>
       <button>Edit</button>
     </div>
   );
--- client/src/pages/TasksPage.jsx
+++ client/src/pages/TasksPage.jsx
@@ -13,13 +13,17 @@ function TasksPage() {
     loadTasks();
   }, []);
 
+  function renderMain() {
+    if(tasks.length === 0) {
+      return <h1>No tasks yet</h1>
+    }
+    return tasks.map((task) => <TaskComponent task={task} key={task.id} />);
+  }
+
   return (
     <div>
       <h1>tasks</h1>
-
-      {tasks.map((task) => (
-        <TaskComponent task={task} key={task.id} />
-      ))}
+      {renderMain()}
     </div>
   );
 }
